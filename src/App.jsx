687d96ee0f81a2003dfafcb5ea32866dec6b4874
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-11 22:23:42 +0530

    Component and Fragment explore

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
 import "./App.jsx";
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <h1>Simple Counter</h1>
+//     </>
+//   );
+// }
 
+function App() {
   return (
     <>
-
-      <h1>Simple Counter</h1>
-
-     
+      <Person></Person>
+      <h2> First Time Using React</h2>
+      <Sports></Sports>
+      <Person></Person>
     </>
   );
+}
+
+function Person() {
+  const name = "kamal";
+
+  return( <p>My Name Is {name}</p>);
+}
+
+function Sports() {
+  return (
+    <div>
+      <h3>Cricket</h3>
+      <ul>
+        <li>Eden Garden</li>
+        <li>Ncr</li>
+      </ul>
+      <p>Playing And Losing</p>
+    </div>
+  );
+}
+
+function Pet() {
+
+  return(
+<h2>Cat</h2>
+<h2>u</h2>
+
+
+
+  )
 }
 
 export default App;
